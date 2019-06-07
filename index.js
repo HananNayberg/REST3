152a6609ce80d3f103cb2b1460a662a05bc7b2a3
Hanan Nayberg <hnayberg@corp.webroot.com>
2019-06-07 08:03:35 -0600

    post, put added

--- index.js
+++ index.js
@@ -1,15 +1,43 @@
 const express = require('express');
 const app = express();
+const uuidv4 = require('uuid/v4');
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req,res)=>{
     res.send("welcome to the app");
 });
 
+let books = [
+    {id: 1, name: "The Hobbit", author: "J.RR Tolkien"},
+    {id: 2, name: "1984", Author: "George Orwell"}
+];
 
+app.get('/books', (req,res) => {
+    return res.send(books);
+});
+
+app.get('/books/:id', (req,res)=> {
+    let book = books[req.params.id]; 
+    return res.send(book);
+});
+
+app.post('/books', (req,res)=>{
+    let id = uuidv4();
+    let book = {
+        id : id,
+        title: req.body.title,
+        author: req.body.author 
+    };
+    books.push(book);
+    return res.send(books);
+});
 
 
 
 const port = 3000;
 app.listen(port, ()=> {
     console.log(`app is runing on port ${port} `);
-})
+});
